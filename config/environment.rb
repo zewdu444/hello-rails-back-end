72cc6d00a0be6ab7749c9f5987e2db964971b1d1
zewdu erkyhun <zewdu444@gmail.com>
2023-06-06 15:21:18 +0300

    Add model for greetings

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
+require_relative "env"
 
 # Initialize the Rails application.
+ENV['DATABASE_PASSWORD'] = DATABASE_PASSWORD
+ENV['DATABASE_USERNAME'] = DATABASE_USERNAME
 Rails.application.initialize!
