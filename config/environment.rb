dd8a35a391487910a7a7846dee267e0c37089e48
zewdu erkyhun <zewdu444@gmail.com>
2023-06-07 12:20:51 +0300

    Fix env setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the Rails application.
 require_relative "application"
-require_relative "env"
 
 # Initialize the Rails application.
-ENV['DATABASE_PASSWORD'] = DATABASE_PASSWORD
-ENV['DATABASE_USERNAME'] = DATABASE_USERNAME
+
 Rails.application.initialize!
